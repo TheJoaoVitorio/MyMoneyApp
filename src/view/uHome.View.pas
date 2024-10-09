d61cd4897cb592cbae399b6843fc2eef33f6052e
TheJoaoVitorio <joaovtr999x@gmail.com>
2024-10-09 17:55:49 -0300

    update

--- src/view/uHome.View.pas
+++ src/view/uHome.View.pas
@@ -7,7 +7,7 @@ uses
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
   FMX.Layouts, FMX.Objects, FMX.Controls.Presentation, FMX.StdCtrls,
   FMX.ListView.Types, FMX.ListView.Appearances, FMX.ListView.Adapters.Base,
-  FMX.ListView;
+  FMX.ListView, uLancamentos.view;
 
 type
   TFHome = class(TForm)
@@ -44,11 +44,20 @@ type
       const AItem: TListViewItem);
     procedure rtBtnHomeAcaoMouseEnter(Sender: TObject);
     procedure rtBtnHomeAcaoMouseLeave(Sender: TObject);
+    procedure lvLancamentoItemClickEx(const Sender: TObject;
+      ItemIndex: Integer; const LocalClickPos: TPointF;
+      const ItemObject: TListItemDrawable);
+    procedure lvLancamentoPaint(Sender: TObject; Canvas: TCanvas;
+      const ARect: TRectF);
+    procedure lblVerTodosClick(Sender: TObject);
+    procedure FormClose(Sender: TObject; var Action: TCloseAction);
   private
-    procedure AddLancamento(ID_LANCAMENTO,DESCRICAO, CATEGORIA : String; VALOR : Double; FOTO: TStream;DT :TDateTime );
+
+
 
   public
-
+      procedure SetupLancamento(ListView: TListView; Item : TListViewItem);
+      procedure AddLancamento(ListView : TListView;ID_LANCAMENTO,DESCRICAO, CATEGORIA : String; VALOR : Double; FOTO: TStream;DT :TDateTime );
   end;
 
 var
@@ -58,58 +67,102 @@ implementation
 
 {$R *.fmx}
 
+procedure TFHome.FormClose(Sender: TObject; var Action: TCloseAction);
+begin
+      if Assigned(FLancamento) then
+        begin
+              FLancamento.DisposeOf;
+              FLancamento := nil;
+        end;
+end;
+
 procedure TFHome.FormShow(Sender: TObject);
 var
   foto : TStream;
 begin
       foto := nil;
 
-      AddLancamento('1','COMPRA','TRANSPORTE', -45,foto,20/08);
+      AddLancamento(lvLancamento,'1','COMPRA','TRANSPORTE', -45,foto,20/08);
+      AddLancamento(lvLancamento,'2','HAMBURGUER','TRANSPORTE', -10,foto,20/08);
 end;
 
 
 
 
-procedure TFHome.lvLancamentoUpdateObjects(const Sender: TObject;
-  const AItem: TListViewItem);
-var
-  txt : TListItemText;
-  img : TListItemImage;
+procedure TFHome.lblVerTodosClick(Sender: TObject);
 begin
-      txt       := TListItemText(AItem.Objects.FindDrawable('txtDESCRICAO'));
+      if not Assigned(FLancamento) then
+        Application.CreateForm(TFLancamento,FLancamento);
 
-      txt.Width := lvLancamento.Width - txt.PlaceOffset.X - 100;
-
-      img := TListItemImage(AItem.Objects.FindDrawable('imgICONE'));
-
-      if lvLancamento.Width < 250 then
-        begin
-              img.Visible       := False;
-              txt.PlaceOffset.X := 2;
-        end
+      FLancamento.Show;
 
 end;
+
+procedure TFHome.lvLancamentoItemClickEx(const Sender: TObject;
+  ItemIndex: Integer; const LocalClickPos: TPointF;
+  const ItemObject: TListItemDrawable);
+begin
+
+//      if TListView(Sender) .Selected <> nil then
+//        begin
+//              if ItemObject is TListItemImage then
+//                begin
+//                      ///......
+//
+//                end;
+//
+//        end;
+
+end;
+
+procedure TFHome.lvLancamentoPaint(Sender: TObject; Canvas: TCanvas;
+  const ARect: TRectF);
+begin
+//      if lvLancamento.Items.Count > 0 then
+//          if lvLancamento.GetItemRect( lvLancamento.Items.Count - 4 ).Bottom <= lvLancamento.Height then
+//              begin
+//                    AddLancamento('003','Supermercado','Transporte',-30,nil,09/10);
+//                    AddLancamento('004','Supermercado','Transporte',-30,nil,09/10);
+//                    AddLancamento('005','Supermercado','Transporte',-30,nil,09/10);
+//                    AddLancamento('006','Supermercado','Transporte',-30,nil,09/10);
+//                    AddLancamento('007','Supermercado','Transporte',-30,nil,09/10);
+//              end;
+end;
+
+
+
 
 procedure TFHome.rtBtnHomeAcaoMouseEnter(Sender: TObject);
 begin
       rtBtnHomeAcao.Fill.Color := $FF7168FE;
 end;
 
+
+
 procedure TFHome.rtBtnHomeAcaoMouseLeave(Sender: TObject);
 begin
       rtBtnHomeAcao.Fill.Color := $FF5A4FFF;
 end;
 
-procedure TFHome.AddLancamento(ID_LANCAMENTO,DESCRICAO, CATEGORIA : String; VALOR : Double; FOTO: TStream;DT :TDateTime );
+
+
+procedure TFHome.AddLancamento(ListView : TListView;
+                               ID_LANCAMENTO,
+                               DESCRICAO,
+                               CATEGORIA : String;
+                               VALOR : Double;
+                               FOTO: TStream;
+                               DT :TDateTime );
 var
   txt : TListItemText;
   img : TListItemImage;
   bmp : TBitmap;
 begin
-     with lvLancamento.Items.Add do
+     with ListView.Items.Add do
         begin
-            txt      := TListItemText(Objects.FindDrawable('txtDESCRICAO'));
-            txt.Text := DESCRICAO;
+            TagString := ID_LANCAMENTO;
+            txt       := TListItemText(Objects.FindDrawable('txtDESCRICAO'));
+            txt.Text  := DESCRICAO;
 
 
             TListItemText(Objects.FindDrawable('txtCATEGORIA')).Text        := CATEGORIA;
@@ -127,6 +180,33 @@ begin
               end;
         end;
 
+end;
+
+
+procedure TFHome.SetupLancamento(ListView: TListView; Item : TListViewItem);
+var
+  txt : TListItemText;
+  img : TListItemImage;
+begin
+      txt       := TListItemText(Item.Objects.FindDrawable('txtDESCRICAO'));
+
+      txt.Width := ListView.Width - txt.PlaceOffset.X - 100;
+
+      img       := TListItemImage(Item.Objects.FindDrawable('imgICONE'));
+
+      if ListView.Width < 250 then
+        begin
+              img.Visible       := False;
+              txt.PlaceOffset.X := 2;
+        end
+end;
+
+
+
+procedure TFHome.lvLancamentoUpdateObjects(const Sender: TObject;
+  const AItem: TListViewItem);
+begin
+      SetupLancamento(FHome.lvLancamento,AItem);
 end;
 
 end.
